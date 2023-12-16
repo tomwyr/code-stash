bdeefcfd77215896f7660c75fc8f6f5ed0eb0304
tomwyr <tomwyr@hotmail.com>
2023-12-16 19:35:41 +0100

    Revert "Update build gradle"
    This reverts commit 189285444881b718fa4acce0f1f8489fdab33fc0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,6 +54,7 @@ kotlin {
     js(IR) {
         browser {
             runTask {
+                mainOutputFileName = "main.bundle.js"
                 sourceMaps = false
                 devServer = KotlinWebpackConfig.DevServer(
                         open = false,
