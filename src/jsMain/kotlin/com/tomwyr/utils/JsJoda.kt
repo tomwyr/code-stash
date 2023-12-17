5ce9aa1973dca2fe3e3e44a7cd8027aa0960538e
tomwyr <tomwyr@hotmail.com>
2023-12-17 14:28:40 +0100

    Add joda core module

--- src/jsMain/kotlin/com/tomwyr/utils/JsJoda.kt
+++ src/jsMain/kotlin/com/tomwyr/utils/JsJoda.kt
@@ -1,7 +1,13 @@
 package com.tomwyr.utils
 
+@JsModule("@js-joda/core")
+@JsNonModule
+external object JsJodaCoreModule
+
+private val jsJodaCore = JsJodaCoreModule
+
 @JsModule("@js-joda/timezone")
 @JsNonModule
 external object JsJodaTimeZoneModule
 
-private val jsJodaTz = JsJodaTimeZoneModule
+private val jsJodaTimeZone = JsJodaTimeZoneModule
