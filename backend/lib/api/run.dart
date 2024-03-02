d1aad0e6a96928e262e6d596f15525942b092e7d
tomwyr <tomwyr@hotmail.com>
2024-03-02 21:35:55 +0100

    Prepare for globe deployment

--- backend/lib/api/run.dart
+++ backend/lib/api/run.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
 
+import '../utils/env.dart';
 import 'team_service.dart';
 
 Future<void> runApi() async {
-  await serve(TeamService().handler, 'localhost', 8080);
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(TeamService().handler);
+  await serve(handler, InternetAddress.anyIPv4, int.parse(Env.appPort));
 }
