7c7c5f0384a0675b25f45d4bf6faa7bf0b4b27b7
Tomasz Wyrowiński <tomwyr@hotmail.com>
2024-11-13 18:44:19 +0100

    Release 0.0.1

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 0.0.1 - 2024-11-13
+
+- Implement core functionality
+- Add `find` and `remove` commands
--- Sources/Cli/Entrypoint.swift
+++ Sources/Cli/Entrypoint.swift
@@ -5,7 +5,7 @@ struct GitBranchCleanerCli: ParsableCommand {
   static let configuration = CommandConfiguration(
     commandName: "gbc",
     abstract: "A command-line utility for cleaning up git branches.",
-    version: "1.0.0",
+    version: "0.0.1",
     subcommands: [Find.self, Remove.self]
   )
 }
