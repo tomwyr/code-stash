3b6514b79737a030cfb3312f37c533f46fec0f0d
Tomasz Wyrowiński <tomwyr@hotmail.com>
2024-11-18 23:36:12 +0100

    Implement find command

--- src/commands/find.ts
+++ src/commands/find.ts
@@ -1,5 +1,52 @@
 import * as vscode from "vscode";
+import * as ffi from "../ffi/ffi";
+import { getProjectRoot } from "../common/project";
+import { Branch } from "../common/types";
 
 export function run() {
-  vscode.window.showInformationMessage("TODO: Implement remove command");
+  let result = ffi.findBranchesToCleanup({
+    projectRoot: getProjectRoot(),
+    branchMaxDepth: 10,
+    refBranchName: "main",
+  });
+
+  switch (result.type) {
+    case "success":
+      onSuccess(result.value);
+      break;
+    case "error":
+      onError(result.value);
+      break;
+    case "unknown":
+      onUnknown(result.value);
+      break;
+  }
 }
+
+function onSuccess(branches: Branch[]) {
+  let formattedBranches = branches.map((branch) => branch.name).join(", ");
+  showInfo(`Branches: ${formattedBranches}`);
+}
+
+async function onError(error: object) {
+  let title = "Finding branches failed";
+  let moreInfo = "More info";
+
+  let item = await showError(title, moreInfo);
+  switch (item) {
+    case moreInfo:
+      showError(title, {
+        modal: true,
+        detail: JSON.stringify(error),
+      });
+      break;
+  }
+}
+
+function onUnknown(result: any) {
+  showWarning(`Unexpected result received: ${result}`);
+}
+
+const showInfo = vscode.window.showInformationMessage;
+const showWarning = vscode.window.showWarningMessage;
+const showError = vscode.window.showErrorMessage;
