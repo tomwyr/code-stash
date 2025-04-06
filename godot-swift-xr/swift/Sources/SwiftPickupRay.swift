55b08efc610d0651d8ce30acb63cf73d7d1c7014
Tomasz Wyrowiński <tomwyr@hotmail.com>
2025-04-06 19:13:13 +0200

    Show pickup ray on trigger press

--- godot-swift-xr/swift/Sources/SwiftPickupRay.swift
+++ godot-swift-xr/swift/Sources/SwiftPickupRay.swift
@@ -7,35 +7,68 @@ class SwiftPickupRay: Node3D, @unchecked Sendable {
   }
 
   var length = Float(10.0)
-  var lineMesh: MeshInstance3D?
 
+  var lineNode: MeshInstance3D?
   var lastBasis: Basis?
   var lastPosition: Vector3?
 
+  var active: Bool { lineNode != nil }
+
   override func _ready() {
-    let mesh = createLineMesh()
-    lineMesh = mesh
-    addChild(node: mesh)
+    setupSignals()
   }
 
   override func _process(delta: Double) {
-    guard let basis = controller?.globalTransform.basis,
-      let position = controller?.globalPosition,
-      basis != lastBasis || position != lastPosition
-    else { return }
-
-    lastBasis = basis
-    lastPosition = position
-
+    guard active, checkLineChanged() else { return }
     let (origin, target) = resolveLineEnds()
     drawLine(from: origin, to: target)
   }
 
-  func createLineMesh() -> MeshInstance3D {
+  func setupSignals() {
+    guard let controller = controller else { return }
+
+    controller.buttonPressed.connect { button in
+      if button == "trigger_click" {
+        self.addLineNode()
+      }
+    }
+    controller.buttonReleased.connect { button in
+      if button == "trigger_click" {
+        self.removeLineNode()
+      }
+    }
+  }
+
+  func addLineNode() {
+    let node = createLineNode()
+    lineNode = node
+    addChild(node: node)
+  }
+
+  func createLineNode() -> MeshInstance3D {
     let mesh = MeshInstance3D()
     mesh.mesh = ImmediateMesh()
     mesh.castShadow = .off
     return mesh
+  }
+
+  func removeLineNode() {
+    lineNode?.queueFree()
+    lineNode = nil
+    lastBasis = nil
+    lastPosition = nil
+  }
+
+  func checkLineChanged() -> Bool {
+    guard let controller = controller else { return false }
+
+    let basis = controller.globalTransform.basis
+    let position = controller.globalPosition
+    let changed = basis != lastBasis || position != lastPosition
+
+    lastBasis = basis
+    lastPosition = position
+    return changed
   }
 
   func resolveLineEnds() -> (Vector3, Vector3) {
@@ -62,7 +95,7 @@ class SwiftPickupRay: Node3D, @unchecked Sendable {
   }
 
   func drawLine(from origin: Vector3, to target: Vector3) {
-    guard let immediateMesh = lineMesh?.mesh as? ImmediateMesh else { return }
+    guard let immediateMesh = lineNode?.mesh as? ImmediateMesh else { return }
 
     let material = ORMMaterial3D()
     material.shadingMode = .unshaded
